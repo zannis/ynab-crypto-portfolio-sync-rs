15e4d59ab8ef7a65549f3d353ebe6a98d88744f1
zannis <zannis@aave.com>
2024-11-18 01:34:13 +0900

    fix: add generated binance api client

---
+++ crates/binance-api/src/models/mod.rs
@@ -0,0 +1,4 @@
+pub mod _sapi_v1_asset_wallet_balance_get_200_response_inner;
+pub use _sapi_v1_asset_wallet_balance_get_200_response_inner::SapiV1AssetWalletBalanceGet200ResponseInner;
+pub mod error;
+pub use error::Error;
